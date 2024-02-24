db051098c3de197fbcbc1f5db631c1754278c819
Jonathas Moura <jonathas.development@gmail.com>
2024-02-24 01:48:59 -0300

    Criação de componentes registrar e usuario

--- pj-gth/src/app/app.module.ts
+++ pj-gth/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuarios/usuario/usuario.component';
+import { RegistrarComponent } from './usuarios/registrar/registrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    RegistrarComponent
   ],
   imports: [
     BrowserModule,
