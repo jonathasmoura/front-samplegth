cbc24aebc9b37142c06915e82968fe12073789ba
Jonathas Moura <jonathas.development@gmail.com>
2024-02-25 04:08:22 -0300

    Implementação para criar registros de usuários

--- pj-gth/src/app/app-routing.module.ts
+++ pj-gth/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './usuarios/usuario/usuario.component';
+import { RegistrarComponent } from './usuarios/registrar/registrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/usuarios/registrar', pathMatch: 'full' },
+  {
+    path: 'usuarios',
+    component: UsuarioComponent,
+    children: [
+      { path: 'usuario', component: UsuarioComponent },
+      { path: 'registrar', component: RegistrarComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
